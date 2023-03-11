e05e29b2face6155864490ebe98db7c29ea83216
Polkampally Keerthana <polkampallykeerthana@client-10-228-95-19.tamulink.tamu.edu>
2023-03-10 19:07:12 -0600

    fixed static pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
     def index
       render
     end
+    def about
+      render
+    end 
+    def contactus
+      render
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def devise_mapping
+        @devise_mapping ||= Devise.mappings[:admin]
+    end
 end
